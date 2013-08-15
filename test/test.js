a4ab57abb46598c65522ecd9698bae1cfbe2ff16
Mat Scales <mscales@google.com>
2013-08-15 22:09:00 +0100

    Basic test outline, need to implement and get some sample data

---
+++ test/test.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var assert = require('chai').assert,
+	decompress = require('../lib/decompress-zip'),
+	pkg = require('../package.json'),
+	samples = [];
+
+describe('Smoke test', function () {
+	it('should find the public interface', function () {
+		assert.strictEqual(decompress.version, pkg.version, 'decompress.version is correct');
+		assert.isFunction(decompress.extract, 'decompress.extract is a function');
+		assert.isFunction(decompress.extract, 'decompress.list is a function');
+	});
+});
+
+describe('Extract', function () {
+	samples.forEach(function (sample) {
+		describe(sample, function () {
+			it('should have the same output files as expected');
+			it('should correctly produce the file contents');
+			it('should set the correct permissions and ownership');
+		});
+	});
+});
