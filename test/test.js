518a20b97c7b4af04fc8f77cffda38b9d540c984
Mat Scales <mscales@google.com>
2013-09-04 18:02:06 +0100

    Fill in the basic tests. I haven't shared the assets yet.

--- test/test.js
+++ test/test.js
@@ -3,7 +3,18 @@
 var assert = require('chai').assert;
 var decompress = require('../lib/decompress-zip');
 var pkg = require('../package.json');
-var samples = [];
+var path = require('path');
+var glob = require('glob');
+var exec = require('child_process').exec;
+var tmp = require('tmp');
+var assetsPath = path.join(__dirname, 'assets');
+
+var samples = glob.sync('*/archive.zip', {cwd: assetsPath});
+
+if (samples.length === 0) {
+    console.log('No sample ZIP files were found. Run "grunt test-files" to download them.');
+    process.exit(1);
+}
 
 describe('Smoke test', function () {
     it('should find the public interface', function () {
@@ -15,9 +26,47 @@ describe('Smoke test', function () {
 
 describe('Extract', function () {
     samples.forEach(function (sample) {
+        var extracted = path.join(path.dirname(sample), 'extracted');
+
         describe(sample, function () {
-            it('should have the same output files as expected');
-            it('should correctly produce the file contents');
+            var tmpDir;
+
+            before(function (done) {
+                tmp.dir({unsafeCleanup: true}, function (err, dir) {
+                    if (err) {
+                        throw err;
+                    }
+
+                    tmpDir = dir;
+                    done();
+                });
+            });
+
+            it('should extract without any errors', function (done) {
+                this.timeout(10000);
+                decompress.extract(path.join(assetsPath, sample), {path: tmpDir})
+                .then(function () {
+                    assert(true, 'success callback executed');
+                    done();
+                })
+                .done();
+            });
+
+            it('should have the same output files as expected', function (done) {
+                exec('diff -qr ' + extracted + ' ' + tmpDir, {cwd: assetsPath}, function (err, stdout, stderr) {
+                    if (err) {
+                        if (err.code === 1) {
+                            assert(false, 'output should match');
+                        } else {
+                            throw err;
+                        }
+                    }
+                    assert.equal(stdout, '');
+                    assert.equal(stderr, '');
+                    done();
+                });
+            });
+
             it('should set the correct permissions and ownership');
         });
     });
