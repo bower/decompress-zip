b7507fa3f470f3dcff1ca57ac0ff6460a1780abf
Jakub Szwacz <jakub@szwacz.com>
2015-01-06 13:09:43 +0100

    Fully automatic test zip files generation

--- test/assets/main-test-pack/generate.js
+++ test/assets/main-test-pack/generate.js
@@ -1,19 +1,15 @@
-// This is how this pack was generated.
-// You can use this code to amend it, or to create other packs for different
-// test (then of course you have manually compress it).
-// This chain of commands does the compression on Ubuntu:
-/*
-node generate.js
-cd extracted
-zip -r -0 ../store.zip .
-zip -r ../deflate.zip .
-cd ..
-
-*/
+// This script generates zip files inside this folder.
 
 'use strict';
 
 var jetpack = require('fs-jetpack');
+var archiver = require('archiver');
+
+var mainDir = jetpack.cwd(__dirname);
+
+// -------------------------------------------------------
+// Generate files and folders which we next will compress
+// -------------------------------------------------------
 
 var fillWithSillyBytes = function (buf) {
     // Very predictable pattern leads to high compression
@@ -28,18 +24,44 @@ var zero = new Buffer(0);
 var oneKib = fillWithSillyBytes(new Buffer(1024));
 var twoMib = fillWithSillyBytes(new Buffer(1024 * 1024 * 2));
 
-// Generate the files structure
-var root = jetpack.dir('extracted', { empty: true });
-root
+var extractedDir = mainDir.dir('extracted', { empty: true });
+extractedDir
+    .dir('empty')
+        .cwd('..')
     .dir('dir1')
         .dir('dir2')
             .file('0B', { content: zero })
             .file('1KiB', { content: oneKib })
-            .file('2MiB', { content: twoMib })
-root
-    .dir('empty');
+            .file('2MiB', { content: twoMib });
+
+// -------------------------------------------------------
+// Generate spec file
+// -------------------------------------------------------
 
 // Generate spec file to which we can later compare
 // our extracted stuff during tests.
-var spec = jetpack.inspectTree('extracted', { checksum: 'sha1' });
-jetpack.write('spec.json', spec, { jsonIndent: 2 });
+var spec = mainDir.inspectTree('extracted', { checksum: 'sha1' });
+mainDir.write('spec.json', spec, { jsonIndent: 2 });
+
+// -------------------------------------------------------
+// Compress to zip files
+// -------------------------------------------------------
+
+var compress = function (dest, useStore) {
+    var output = mainDir.createWriteStream(dest);
+    output.on('close', function () {
+        console.log('Archive ' + dest + ' created.');
+    });
+    var archive = archiver('zip', { store: useStore });
+    archive.on('error', function (err){
+        console.log(err);
+    });
+    archive.pipe(output);
+    archive.bulk([
+        { expand: true, cwd: extractedDir.path(), src: ['**'] }
+    ]);
+    archive.finalize();
+};
+
+compress('store.zip', true);
+compress('deflate.zip', false);
